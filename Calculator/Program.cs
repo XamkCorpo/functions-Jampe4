f6c76147548648623a7ccae9b2691533cef08428
Jasse Kuutsuo <fjaku012@edu.xamk.fi>
2025-09-24 10:23:24 +0300

    Final

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,13 +1,11 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Calculator
+﻿namespace Calculator
 {
     internal class Program
     {
         /// <summary>
         /// Kysytään käyttäjältä lukua 1-4 välillä
         /// </summary>
-        /// <returns>palauttaa valinnan</returns>
+        /// <returns> palauttaa valinnan </returns>
         static int Valitselaskutoimitus()
         {
             // Tarkistetaan että syöte on 1, 2, 3 tai 4
@@ -19,7 +17,7 @@ namespace Calculator
                 {
                     break;
                 }
-
+                // Ilmoitetaan jos syöte on virheellinen
                 else
                 {
                     Console.WriteLine("Virheellinen syöte. Valitse luku 1-4 väliltä");
@@ -34,7 +32,7 @@ namespace Calculator
         /// </summary>
         /// <param name="Luku1"></param>
         /// <param name="Luku2"></param>
-        /// <returns>palauttaa tuloksen</returns>
+        /// <returns> palauttaa tuloksen </returns>
         static double Yhteenlasku(double Luku1, double Luku2)
         {
             double Tulos = 0;
@@ -47,7 +45,7 @@ namespace Calculator
         /// </summary>
         /// <param name="Luku1"></param>
         /// <param name="Luku2"></param>
-        /// <returns>palauttaa tuloksen</returns>
+        /// <returns> palauttaa tuloksen </returns>
         static double Vahennyslasku(double Luku1, double Luku2)
         {
             double Tulos = 0;
@@ -59,38 +57,43 @@ namespace Calculator
         /// </summary>
         /// <param name="Luku1"></param>
         /// <param name="Luku2"></param>
-        /// <returns>palauttaa tuloksen</returns>
+        /// <returns> palauttaa tuloksen </returns>
         static double Kertolasku(double Luku1, double Luku2)
         {
             double Tulos = 0;
             Tulos = Luku1 * Luku2;
             return Tulos;
         }
+
         /// <summary>
         /// Laskee kahden numeron osamäärän
         /// </summary>
         /// <param name="Luku1"></param>
         /// <param name="Luku2"></param>
-        /// <returns>palauttaa tuloksen</returns>
+        /// <returns> palauttaa tuloksen </returns>
         static double Jakolasku(double Luku1, double Luku2)
         {
+            // Tarkistetaan että jakolaskussa ei jaeta nollalla
             if (Luku2 == 0 || Luku1 == 0)
             {
                 Console.WriteLine("Nollalla ei voi jakaa.");
                 return 0;
             }
+
+            // muuten suoritetaan jakolasku
             double Tulos = 0;
             Tulos = Luku1 / Luku2;
             return Tulos;
         }
+
         /// <summary>
         /// Kysytään käyttäjältä luku ja varmistetaan että se on kelvollinen
         /// </summary>
-        /// <returns>Palautetaan kelvollinen luku</returns>
+        /// <returns> Palautetaan kelvollinen luku </returns>
         static double Kysyluku()
         {
             double Luku = 0;
-            
+
             while (true)
             {
                 string Syöte = Console.ReadLine();
@@ -99,54 +102,61 @@ namespace Calculator
                 {
                     break;
                 }
+                // Ilmoitetaan jos syöte on virheellinen
                 else
                 {
-                    Console.WriteLine("Virheellinen syöte. Anna kelvollinen luku. Käytä desimaaleissa pilkkua , ei pistettä .");
+                    Console.WriteLine("Virheellinen syöte. Anna kelvollinen luku. Käytä desimaaleissa pilkkua' , ' EI pistettä' . '");
                     continue;
                 }
             }
             return Luku;
-            
+
         }
+
         /// <summary>
         /// Tulostetaan laskutoimituksen tulos
         /// </summary>
         /// <param name="Tulos"></param>
         static void Tulostatulos(double Tulos)
         {
-            Console.WriteLine($"Tulos = {Tulos}");
+            Console.WriteLine($"\nTulos = {Tulos}");
         }
+
+
         static void Main(string[] args)
         {
 
             double Tulos = 0;
             int valinta = 0;
+
             // Kysytään käyttäjältä laskutoimitusta
             Console.WriteLine("Valitse laskutoimitus: \n1. Yhteenlasku\n2. Vähennyslasku\n3. Kertolasku\n4. Jakolasku");
             valinta = Valitselaskutoimitus();
-            
+
             // Kysytään käyttäjältä kahta lukua
-            Console.WriteLine("Anna ensimmäinen luku:");
+            Console.WriteLine("\nAnna ensimmäinen luku:");
             double Luku1 = Kysyluku();
-            Console.WriteLine("Anna toinen luku:");
+            Console.WriteLine("\nAnna toinen luku:");
             double Luku2 = Kysyluku();
 
-            // Suoritetaan laskutoimitus valinnan mukaan
+            // Suoritetaan laskutoimitus valinnan mukaan ja tulostetaan tulos
             switch (valinta)
             {
                 case 1:
                     Tulos = Yhteenlasku(Luku1, Luku2);
                     Tulostatulos(Tulos);
                     break;
-                
+
                 case 2:
                     Tulos = Vahennyslasku(Luku1, Luku2);
                     Tulostatulos(Tulos);
                     break;
+
                 case 3:
                     Tulos = Kertolasku(Luku1, Luku2);
                     Tulostatulos(Tulos);
                     break;
+
                 case 4:
                     Tulos = Jakolasku(Luku1, Luku2);
                     Tulostatulos(Tulos);
